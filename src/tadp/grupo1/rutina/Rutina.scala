65bd183f87f1816573889b457c9cd200493366dd
Alejandro <alejandro.nosenzo@gmail.com>
2015-06-30 23:41:11 -0300

    Revert "Actividad devuelve Try y Rutina con que joraca lo hago flatMap Map no"
    This reverts commit 098c62ac65b657149b98f9bdf49324a7403ecdc5.

--- src/tadp/grupo1/rutina/Rutina.scala
+++ src/tadp/grupo1/rutina/Rutina.scala
@@ -13,18 +13,17 @@ case class Rutina(nombre: String, listaActividades: List[Actividad]) {
 
   def realizarRutina(pokemon: Pokemon): Pokemon = {
 
-    def pokemonDespuesDeRutina = listaActividades.foldLeft(Try(Try(pokemon))) {
-//      (pokemonAnterior, actividadActual) => pokemonAnterior.flatMap( poke => poke.realizarActividad(actividadActual))
-      (pokemonAnterior, actividadActual) => pokemonAnterior.map( poke => poke.realizarActividad(actividadActual))
-    }
-    
-    pokemonDespuesDeRutina match {
-      case Failure(exception) => throw exception
-      case Success(pokemonDespuesDeRutina) => pokemonDespuesDeRutina
-    }
-
-//    listaActividades.foldLeft(pokemon) {
-//      (pokemonAnterior, actividadActual) => pokemonAnterior.realizarActividad(actividadActual)
+//    def pokemonDespuesDeRutina = listaActividades.foldLeft(Try(pokemon)) {
+//      (pokemonAnterior, actividadActual) => pokemonAnterior.map(_.realizarActividad(actividadActual))
 //    }
+//    
+//    pokemonDespuesDeRutina match {
+//      case Failure(exception) => throw exception
+//      case Success(pokemonDespuesDeRutina) => pokemonDespuesDeRutina
+//    }
+
+    listaActividades.foldLeft(pokemon) {
+      (pokemonAnterior, actividadActual) => pokemonAnterior.realizarActividad(actividadActual)
+    }
   }
 }
